818cb983d459f2346c33de78bf58305c3a2d6f78
amora7741 <mora7741@gmail.com>
2024-04-01 11:52:14 -0700

    Remove unused packages, add helmet

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
+const helmet = require('helmet');
 
 var indexRouter = require('./routes/index');
 var coordinatesRouter = require('./routes/coordinates');
@@ -20,11 +21,8 @@ db.on('error', console.error.bind(console, 'mongo connection error'));
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
 app.use(cors());
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
