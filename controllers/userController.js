185a3be992abfdd320588afb5ae0d7f7e405c476
amora7741 <mora7741@gmail.com>
2024-04-01 12:58:47 -0700

    Only allow length 10 nicknames

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,33 +1,48 @@
 const User = require('../models/User');
 const asyncHandler = require('express-async-handler');
 const { DateTime } = require('luxon');
+const { body, validationResult } = require('express-validator');
 
 const getAllUsers = asyncHandler(async (req, res, next) => {
   const allUsers = await User.find().sort({ duration: 1 });
   res.json(allUsers);
 });
 
-const addUser = asyncHandler(async (req, res, next) => {
-  // Create a DateTime object in UTC
-  const nowUTC = DateTime.utc();
+const addUser = [
+  body('nickname')
+    .trim()
+    .isLength({ min: 1, max: 10 })
+    .escape()
+    .withMessage('Nickname must be 1-10 characters.'),
 
-  // Adjust to local time zone for display (if needed)
-  const nowLocal = nowUTC.setZone('local');
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
 
-  const user = new User({
-    nickname: req.body.nickname,
-    duration: req.body.duration,
-    date: nowUTC.toJSDate(), // Save the UTC date in the database
-    // Display the local time zone
-    localDate: nowLocal.toLocaleString({
-      month: 'long',
-      day: '2-digit',
-      year: 'numeric',
-    }),
-  });
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-  await user.save();
-  res.status(201).json(user);
-});
+    // Create a DateTime object in UTC
+    const nowUTC = DateTime.utc();
+
+    // Adjust to local time zone for display (if needed)
+    const nowLocal = nowUTC.setZone('local');
+
+    const user = new User({
+      nickname: req.body.nickname,
+      duration: req.body.duration,
+      date: nowUTC.toJSDate(), // Save the UTC date in the database
+      // Display the local time zone
+      localDate: nowLocal.toLocaleString({
+        month: 'long',
+        day: '2-digit',
+        year: 'numeric',
+      }),
+    });
+
+    await user.save();
+    res.status(201).json(user);
+  }),
+];
 
 module.exports = { getAllUsers, addUser };
